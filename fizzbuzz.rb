509bb36d8f0885bc4a7a93b9cc5929b71efb1cee
lieby weintraub <liebahinda@gmail.com>
2019-01-25 03:36:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
+def fizzbuzz(int)
   if int % 3 == 0 
    "Fizz"
 
@@ -12,5 +12,5 @@ elsif int % 5 == 0
  
   else !(int % 3 || 5 == 0)
    nil
-
+end 
 end
