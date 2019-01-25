e65b8734bd0097bccb3351e85deccf33b33426cc
lieby weintraub <liebahinda@gmail.com>
2019-01-25 03:17:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,18 +3,14 @@
 def fizzbuzz(int)
   if int % 3 == 0 
    "Fizz"
-end
-end 
- def fizzbuzz(int) 
-if int % 5 == 0 
-  "Buzz"
-end 
-end 
-def fizzbuzz(int)
-  if int % 3 & 5 == 0
-    "FizzBuzz"
-  end 
-end 
 
+elsif int % 5 == 0
+  "Buzz"
+
+  elsif int % 3 & 5 == 0
+ "FizzBuzz"
  
- 
+  else !(int % 3 || 5 == 0)
+   nil
+  end 
+end
